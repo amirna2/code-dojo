b4896ffa1b60d55be2e8e41ece80365c68ce668d
Amir Nathoo <amir.nth@gmail.com>
2020-07-21 14:06:23 -0700

    Implemented PokerHand

--- poker-java/src/com/amirnathoo/poker/PokerHand.java
+++ poker-java/src/com/amirnathoo/poker/PokerHand.java
@@ -1,14 +1,18 @@
 package com.amirnathoo.poker;
 
 public class PokerHand {
-	
-	private Card[] cards;
-	
+
+	public static final int HAND_SIZE = 5;
+
+	private Card[] cards = new Card[HAND_SIZE];
+
 	public PokerHand(Card cards[]) {
-		this.cards = cards;
-		
+		for (int i = 0; i < HAND_SIZE; i++) {
+			this.cards[i] = cards[i];
+		}
+		sortByRankAscending();
 	}
-	
+
 	public Card[] getCards() {
 		return cards;
 	}
@@ -16,47 +20,134 @@ public class PokerHand {
 	public void setCards(Card[] cards) {
 		this.cards = cards;
 	}
-	
-	boolean isRoyalFlush() {
-		return false;
+
+	public boolean isRoyalFlush() {
+		// is straight, flush and the last card is an ace.
+		boolean r1, r2, r3;
+		r1 = isFlush();
+		r2 = isStraight();
+		r3 = (cards[HAND_SIZE-1].getRank() == Card.A);
+		return (r1 && r2 && r3);
 	}
-	
-	boolean isStriaghtFlush() {
-		return false;
+
+	public boolean isStraightFlush() {
+		return (isFlush() && isStraight());
 	}
-	
-	boolean isFourOfaKind() {
-		return false;
+
+	public boolean isFourOfaKind() {
+		boolean r1, r2;
+
+		// check for x.x.x.x.y || y.x.x.x.x
+		r1 = (cards[0].getRank() == cards[1].getRank()) && (cards[1].getRank() == cards[2].getRank()) && (cards[2].getRank() == cards[3].getRank());
+		r2 = (cards[1].getRank() == cards[2].getRank()) && (cards[2].getRank() == cards[3].getRank()) && (cards[3].getRank() == cards[4].getRank());
+
+		return (r1 || r2);
 	}
-	
-	boolean isFullHouse() {
-		return false;
+
+	public boolean isFullHouse() {
+		boolean r1, r2;
+		// check for x.x.y.y.y || x.x.x.y.y
+		r1 = (cards[0].getRank() == cards[1].getRank()) &&
+			 (cards[2].getRank() == cards[3].getRank()) &&
+			 (cards[3].getRank() == cards[4].getRank());
+
+		r2 = (cards[0].getRank() == cards[1].getRank()) &&
+		     (cards[1].getRank() == cards[2].getRank()) &&
+			 (cards[3].getRank() == cards[4].getRank());
+
+		return (r1 || r2);
 	}
-	
-	boolean isFlush() {
-		return false;
+
+	public boolean isFlush() {
+		boolean r;
+		r = cards[0].getSuit() == cards[1].getSuit() && cards[1].getSuit() == cards[2].getSuit()
+				&& cards[2].getSuit() == cards[3].getSuit() && cards[3].getSuit() == cards[4].getSuit();
+
+		return r;
 	}
-	
-	boolean isStraight() {
-		return false;
+
+	public boolean isStraight() {
+		boolean r = true;
+		for (int i = 0; i < HAND_SIZE - 1; i++) {
+			if (cards[i].getRank() != (cards[i + 1].getRank() - 1)) {
+				r = false;
+				break;
+			}
+		}
+		return r;
+
 	}
-	
-	boolean isThreeOfAKind() {
-		return false;
+
+	public boolean isThreeOfAKind() {
+		boolean r = false;
+		if(isFourOfaKind() || isFullHouse()) {
+			return r;
+		}
+		// x.x.x.y.z || y.x.x.x.z || y.z.x.x.x
+		if ( (cards[0].getRank() == cards[1].getRank() && cards[1].getRank() == cards[2].getRank()) ||
+				  (cards[1].getRank() == cards[2].getRank() && cards[2].getRank() == cards[3].getRank()) ||
+				  (cards[3].getRank() == cards[2].getRank() && cards[3].getRank() == cards[4].getRank())) {
+	        r = true;
+		}
+		return r;
 	}
-	
-	boolean isTwoPairs() {
-		return false;
-	}
-	
-	boolean isHighCard() {
-		return false;
-	}
-	
-	void sortHandBySuit() {
-	}
-	
-	void sortHandByRank() {
+
+	public boolean isTwoPairs() {
+		boolean r = false;
+		if(isFourOfaKind()) {
+			return r;
+		} 
 		
+		// x.x.y.y.z || x.y.y.z.z 
+			
+		r = (cards[0].getRank() == cards[1].getRank() && cards[2].getRank() == cards[3].getRank()) ||
+			(cards[1].getRank() == cards[2].getRank() && cards[3].getRank() == cards[4].getRank());
+
+		return r;
+	}
+
+	public boolean isOnePair() {
+		boolean r = false;
+		if(isThreeOfAKind() || isFourOfaKind() || isFullHouse() || isTwoPairs())
+		{
+			return r;
+		}
+		// w.w.x.y.z || x.w.w.y.z || x.y.w.w.z || x.y.z.w.w
+		if ( cards[0].getRank() == cards[1].getRank() || cards[1].getRank() == cards[2].getRank() ||
+			 cards[2].getRank() == cards[3].getRank() || cards[3].getRank() == cards[4].getRank())
+		{
+			r = true;
+		}
+		return r;	
+	}
+
+	public boolean isHighCard() {
+		boolean r = false;
+
+		if (!isRoyalFlush() && !isFlush() && !isStraight() && !isFullHouse() && !isStraightFlush() && !isThreeOfAKind()
+				&& !isFourOfaKind() && !isTwoPairs() && !isOnePair()) {
+			r = true;
+		}
+		return r;
+	}
+
+
+	// Sort the hand by rank: e.g 2,3,4,...,10,J=11, Q=12,K=13,A=14
+	public void sortByRankAscending() {
+		int smallest;
+		Card c;
+		for (int i = 0; i < HAND_SIZE - 1; i++) {
+			// find smallest card
+			smallest = i;
+			for (int j = i + 1; j < HAND_SIZE; j++) {
+				if (cards[j].getRank() < cards[smallest].getRank()) {
+					smallest = j;
+				}
+			}
+			// swap it to the right spot
+			c = cards[i];
+			cards[i] = cards[smallest];
+			cards[smallest] = c;
+		}
 	}
 }
--- poker-java/src/com/amirnathoo/poker/test/PokerHandTest.java
+++ poker-java/src/com/amirnathoo/poker/test/PokerHandTest.java
@@ -10,6 +10,63 @@ import com.amirnathoo.poker.PokerHand;
 
 class PokerHandTest {
 
+	Card pair[] = {
+		 new Card(Card.A, Card.CLUB),
+		 new Card(Card.A, Card.HEART),
+		 new Card(Card.Two, Card.DIAMOND),
+		 new Card(Card.J, Card.SPADE),
+		 new Card(Card.Five, Card.CLUB) 
+	};
+	
+	Card doublePair[] = {
+		 new Card(Card.A, Card.CLUB),
+		 new Card(Card.A, Card.HEART),
+		 new Card(Card.Q, Card.DIAMOND),
+		 new Card(Card.Q, Card.SPADE),
+		 new Card(Card.Five, Card.CLUB) 
+	};
+	
+	Card ThreeOfAKind[] = {
+			 new Card(Card.A, Card.CLUB),
+			 new Card(Card.Q, Card.HEART),
+			 new Card(Card.Q, Card.DIAMOND),
+			 new Card(Card.Q, Card.SPADE),
+			 new Card(Card.Five, Card.CLUB) 
+	};
+	
+	Card FourOfAKind[] = {
+			 new Card(Card.A, Card.CLUB),
+			 new Card(Card.Q, Card.HEART),
+			 new Card(Card.Q, Card.DIAMOND),
+			 new Card(Card.Q, Card.SPADE),
+			 new Card(Card.Q, Card.CLUB) 
+	};
+	
+	Card flush[] = {
+			 new Card(Card.K, Card.HEART),
+			 new Card(Card.Q, Card.HEART),
+			 new Card(Card.Two, Card.HEART),
+			 new Card(Card.A, Card.HEART),
+			 new Card(Card.J, Card.HEART) 
+	};
+	
+	Card royalFlush[] = {
+			 new Card(Card.Ten, Card.CLUB),
+			 new Card(Card.K, Card.CLUB),
+			 new Card(Card.Q, Card.CLUB),
+			 new Card(Card.J, Card.CLUB),
+			 new Card(Card.A, Card.CLUB) 
+	};
+	
+	
+	Card highCard[] = {
+			 new Card(Card.K, Card.HEART),
+			 new Card(Card.Two, Card.HEART),
+			 new Card(Card.Five, Card.CLUB),
+			 new Card(Card.Three, Card.SPADE),
+			 new Card(Card.Six, Card.DIAMOND) 
+	};
+	
 	@DisplayName("Creates a poker hand with 5 cards")
 	@Test
 	void createAPokerHand() {
@@ -25,6 +82,36 @@ class PokerHandTest {
 		assertNotNull(hand.getCards());
 		assertEquals(hand.getCards()[0], new Card(Card.A, Card.CLUB));
 		
+	}
+	
+	@DisplayName("Sort hand by rank")
+	@Test
+	void sortHandByRankAscending() {
+		Card cards[] = { new Card(Card.A, Card.CLUB),
+				 new Card(Card.A, Card.HEART),
+				 new Card(Card.Two, Card.DIAMOND),
+				 new Card(Card.J, Card.SPADE),
+				 new Card(Card.Five, Card.CLUB) };
+		PokerHand hand = new PokerHand(cards);
+		hand.sortByRankAscending();
+		assertEquals(hand.getCards()[0].getRank(), Card.Two);
+		assertEquals(hand.getCards()[PokerHand.HAND_SIZE - 1].getRank(), Card.A);
+		
+
+	}
+	
+	@DisplayName("Is a high card")
+	@Test
+	void testIsHighHand() {
+		PokerHand hand = new PokerHand(highCard);
+		assertTrue(hand.isHighCard());		
+	}
+	
+	@DisplayName("Is not a high card")
+	@Test
+	void testIsNotHighHand() {
+		PokerHand hand = new PokerHand(flush);
+		assertFalse(hand.isHighCard());		
 	}
 
 }
